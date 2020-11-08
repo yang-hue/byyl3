e5f28cf473bc2918c389ece10642ac6ec2ad6458
unknown <you@example.com>
2020-11-08 11:05:21 +0800

    a

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -382,12 +382,14 @@ public final class Analyser {
 
         while (true) {
             // 预读可能是运算符的 token
-            Token op = next();
+            Token op = peek();
 
             if (op.getTokenType() != TokenType.Mult && op.getTokenType() != TokenType.Div) {
                 break;
             }
             // 运算符
+            next();
+            
             analyseFactor();
             // 因子
 
